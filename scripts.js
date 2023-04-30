aae993b4bdb6ed68292f819aa055b3dd92777396
giacomonileo <leogiacomoni29@gmail.com>
2023-04-30 09:47:56 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function trocacor(cor) {
+    let circulo = document.querySelector(".circulo")
+    circulo.style.background = cor
+
+}
+
+function trocaImagem(img) {
+    let imgiphone = document.querySelector(".iphone")
+    imgiphone.src = img
+
+}
